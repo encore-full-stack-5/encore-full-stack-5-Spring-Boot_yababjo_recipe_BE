5f1350a4993aa5e69581e6b887cf3027d7314c50
m48483 <71882579+m48483@users.noreply.github.com>
2024-04-16 21:02:25 +0900

    임시

--- src/main/java/com/example/recipe/comment/dto/response/CommentResponse.java
+++ src/main/java/com/example/recipe/comment/dto/response/CommentResponse.java
@@ -1,4 +1,18 @@
 package com.example.recipe.comment.dto.response;
 
-public record CommentResponse() {
+import com.example.recipe.global.domain.dto.CommentDto;
+import com.example.recipe.global.domain.dto.RecipeDto;
+import com.example.recipe.global.domain.dto.UserDto;
+import com.example.recipe.global.domain.entity.Comment;
+import com.example.recipe.global.domain.entity.User;
+
+public record CommentResponse(
+        Long id, String content, int rating,
+        UserDto user, RecipeDto recipe
+) {
+    public static CommentResponse from(Comment comment) {
+        User user = comment.getUser();
+        UserDto userDto =
+                new UserDto(user.)
+    }
 }
