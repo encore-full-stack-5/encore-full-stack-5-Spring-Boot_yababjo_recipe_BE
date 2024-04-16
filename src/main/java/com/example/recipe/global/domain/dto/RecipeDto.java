89341155234585141c2ed4422165dc70da7175e1
son7877 <hongbeom7877@naver.com>
2024-04-16 19:23:50 +0900

    sauce&recipe

--- src/main/java/com/example/recipe/global/domain/dto/RecipeDto.java
+++ src/main/java/com/example/recipe/global/domain/dto/RecipeDto.java
@@ -1,6 +1,16 @@
 package com.example.recipe.global.domain.dto;
 
-public record RecipeDto(
+import java.time.LocalDateTime;
 
+public record RecipeDto(
+    Long id,
+    String name,
+    String tip,
+    int servingSize,
+    int cookingLevel,
+    double cookingTime,
+    LocalDateTime recipeCreateDate,
+    double averageRating,
+    int recipeViews
 ) {
 }
--- src/main/java/com/example/recipe/global/domain/entity/Sauce.java
+++ src/main/java/com/example/recipe/global/domain/entity/Sauce.java
@@ -1,4 +1,31 @@
 package com.example.recipe.global.domain.entity;
 
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.Id;
+
+import java.util.List;
+
+@Entity
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Builder
+@Table(name = "FOOD_SAUCE")
 public class Sauce {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "SAUCE_ID", nullable = false)
+    private int id;
+
+    @Column(name = "SAUCE_NAME", nullable = false)
+    private String sauceName;
+
+    @OneToMany(mappedBy = "sauce")
+    private List<SauceRecipeBridge> sauceRecipeBridges;
+
 }
