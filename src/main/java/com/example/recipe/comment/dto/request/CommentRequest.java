b29389335836a9a947af46e1f5839a4117689ff3
m48483 <71882579+m48483@users.noreply.github.com>
2024-04-16 21:02:25 +0900

    임시

--- src/main/java/com/example/recipe/comment/dto/request/CommentRequest.java
+++ src/main/java/com/example/recipe/comment/dto/request/CommentRequest.java
@@ -1,4 +1,24 @@
 package com.example.recipe.comment.dto.request;
 
-public record CommentRequest() {
+import com.example.recipe.global.domain.entity.Comment;
+import com.example.recipe.global.domain.entity.Recipe;
+import com.example.recipe.global.domain.entity.User;
+
+import java.time.LocalDateTime;
+
+public record CommentRequest(
+        String content,
+        int rating,
+        Long userId,
+        Long recipeId
+) {
+    public Comment toEntity(User user, Recipe recipe) {
+        return Comment.builder()
+                .content(content)
+                .rating(rating)
+                .recipe(recipe)
+                .user(user)
+                .createdAt(LocalDateTime.now())
+                .build();
+    }
 }
