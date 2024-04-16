cb4f058ae73ccf47d70ccea9e80e9edf5acc014c
m48483 <71882579+m48483@users.noreply.github.com>
2024-04-16 18:04:32 +0900

    comment repo, DTO 추가

--- src/main/java/com/example/recipe/global/domain/repository/CommentRepository.java
+++ src/main/java/com/example/recipe/global/domain/repository/CommentRepository.java
@@ -1,4 +1,7 @@
 package com.example.recipe.global.domain.repository;
 
-public interface CommentRepository {
+import com.example.recipe.global.domain.entity.Comment;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface CommentRepository extends JpaRepository<Comment, Long> {
 }
