71723a8d57c82abf1fb2faabf5e995b92cc5ef95
son7877 <hongbeom7877@naver.com>
2024-04-18 08:43:16 +0900

    login

--- src/main/java/com/example/recipe/user/controller/UserController.java
+++ src/main/java/com/example/recipe/user/controller/UserController.java
@@ -1,11 +1,12 @@
 package com.example.recipe.user.controller;
 
+import com.example.recipe.user.dto.request.UserSignInRequest;
+import com.example.recipe.user.dto.request.UserSignUpRequest;
 import com.example.recipe.user.dto.response.UserResponse;
 import com.example.recipe.user.service.UserService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequiredArgsConstructor
@@ -14,6 +15,14 @@ public class UserController {
     @Autowired
     private final UserService userService;
 
-//    public UserResponse Login()
+    @PostMapping("/signup")
+    public void signUp(@RequestBody UserSignUpRequest req){
+        userService.signUp(req);
+    }
+    @PostMapping("/login")
+    public UserResponse Login(@RequestBody UserSignInRequest req){
+        return userService.login(req);
+    }
+
 
 }
