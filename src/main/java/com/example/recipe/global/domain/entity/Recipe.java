eb0e37bd71d8650e8ff274d0109a098393c8d566
son7877 <hongbeom7877@naver.com>
2024-04-17 17:41:11 +0900

    recipeupdate

--- src/main/java/com/example/recipe/global/domain/entity/Recipe.java
+++ src/main/java/com/example/recipe/global/domain/entity/Recipe.java
@@ -20,7 +20,7 @@ public class Recipe {
 
     @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "FOOD_RECIPE_ID", nullable = false)
-    private int id;
+    private Long id;
 
     @Column(name = "FOOD_RECIPE_FOOD_NAME", nullable = false)
     private String foodName;
@@ -29,7 +29,7 @@ public class Recipe {
     private String recipeTitle;
 
     @Column(name = "FOOD_RECIPE_COOKING_TIP")
-    private int cookingTip;
+    private String cookingTip;
 
     @Column(name = "FOOD_RECIPE_SERVING_SIZE")
     private int servingSize;
