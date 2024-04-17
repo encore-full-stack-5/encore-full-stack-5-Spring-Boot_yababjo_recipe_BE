63e9e9e3dd3c75ce9a7db4922dc4b69b699a02a7
m48483 <71882579+m48483@users.noreply.github.com>
2024-04-17 17:56:38 +0900

    임시

--- src/main/java/com/example/recipe/comment/service/CommentServiceImpl.java
+++ src/main/java/com/example/recipe/comment/service/CommentServiceImpl.java
@@ -1,4 +1,18 @@
 package com.example.recipe.comment.service;
 
-public class CommentServiceImpl {
+import com.example.recipe.comment.dto.request.CommentRequest;
+import com.example.recipe.comment.dto.response.CommentResponse;
+
+import java.util.List;
+
+public class CommentServiceImpl implements CommentService{
+    @Override
+    public void save(CommentRequest req) {
+
+    }
+
+    @Override
+    public List<CommentResponse> getAll() {
+        return List.of();
+    }
 }
