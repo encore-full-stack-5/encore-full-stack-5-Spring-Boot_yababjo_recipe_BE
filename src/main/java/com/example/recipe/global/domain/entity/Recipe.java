bd9ba2773d2ebf5e9e48a43f1a8d0eafd2bea5fe
son7877 <hongbeom7877@naver.com>
2024-04-17 17:38:18 +0900

    recipeUpdate

--- src/main/java/com/example/recipe/global/domain/entity/Recipe.java
+++ src/main/java/com/example/recipe/global/domain/entity/Recipe.java
@@ -19,7 +19,7 @@ public class Recipe {
 
     @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "FOOD_RECIPE_ID", nullable = false)
-    private int id;
+    private Long id;
 
     @Column(name = "FOOD_RECIPE_FOOD_NAME", nullable = false)
     private String foodName;
@@ -28,7 +28,7 @@ public class Recipe {
     private String recipeTitle;
 
     @Column(name = "FOOD_RECIPE_COOKING_TIP")
-    private int cookingTip;
+    private String cookingTip;
 
     @Column(name = "FOOD_RECIPE_SERVING_SIZE")
     private int servingSize;
--- src/main/java/com/example/recipe/global/domain/tagIng/TagIngServiceImpl.java
+++
@@ -1,5 +0,0 @@
-package com.example.recipe.global.domain.tagIng;
-
-public class TagIngServiceImpl {
-
-}
