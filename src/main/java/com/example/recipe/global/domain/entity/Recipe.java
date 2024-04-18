f35196ea41e6b1fa50c213e007a4e4157466801a
m48483 <71882579+m48483@users.noreply.github.com>
2024-04-18 16:46:16 +0900

    충돌 수정 Recipe, Sauce ID 빨간 줄 해결

--- src/main/java/com/example/recipe/global/domain/entity/Recipe.java
+++ src/main/java/com/example/recipe/global/domain/entity/Recipe.java
@@ -6,7 +6,7 @@ import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import org.springframework.cglib.core.Local;
-import org.springframework.data.annotation.Id;
+
 
 import java.time.LocalDateTime;
 import java.util.List;
