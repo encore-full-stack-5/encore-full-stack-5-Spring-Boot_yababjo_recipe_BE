15f1fb37b6cd188dcb01bad87b32c9d7294fe18d
giraffenostrils <dreamthre@gamil.com>
2024-04-19 20:52:42 +0900

    update3

--- src/main/java/com/example/recipe/tag/service/TagService.java
+++ src/main/java/com/example/recipe/tag/service/TagService.java
@@ -2,9 +2,11 @@ package com.example.recipe.tag.service;
 
 import com.example.recipe.global.domain.entity.Tag;
 import com.example.recipe.tag.dto.Request.TagRequest;
+import com.example.recipe.tag.dto.Response.TagResponse;
 
 import java.util.List;
 
 public interface TagService{
   void save(TagRequest req);
+  List<Tag> showTag(TagResponse response);
 }
--- src/main/java/com/example/recipe/tag/service/TagServiceImpl.java
+++ src/main/java/com/example/recipe/tag/service/TagServiceImpl.java
@@ -3,6 +3,7 @@ package com.example.recipe.tag.service;
 import com.example.recipe.global.domain.entity.Tag;
 import com.example.recipe.global.domain.repository.TagRepository;
 import com.example.recipe.tag.dto.Request.TagRequest;
+import com.example.recipe.tag.dto.Response.TagResponse;
 import lombok.NoArgsConstructor;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -20,6 +21,13 @@ public class TagServiceImpl implements TagService {
     @Override
     public void save(TagRequest req) {
 
+       Tag tag =  tagRepository.save(new Tag());
+
+    }
+
+    @Override
+    public List<Tag> showTag(TagResponse response) {
+        return null;
 
     }
 
