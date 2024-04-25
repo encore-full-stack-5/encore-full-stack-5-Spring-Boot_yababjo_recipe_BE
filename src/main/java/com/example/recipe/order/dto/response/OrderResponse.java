03a18904f34b8969c7d4970c6fee31e338d541bb
giraffenostrils <dreamthre@gamil.com>
2024-04-25 17:49:07 +0900

    dev add

--- src/main/java/com/example/recipe/order/dto/response/OrderResponse.java
+++ src/main/java/com/example/recipe/order/dto/response/OrderResponse.java
@@ -3,6 +3,7 @@ package com.example.recipe.order.dto.response;
 import com.example.recipe.global.domain.entity.CookingOrder;
 
 public record OrderResponse(
+
         Long id, int order, String instruction, Long recipeId
 ) {
     public static OrderResponse from(CookingOrder order) {
